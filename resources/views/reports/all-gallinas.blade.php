f9f8e96dff4b19300542310010bf8339949a60ab
gluis <luisfgb919@gmail.com>
2024-07-11 10:28:19 -0400

    Avances

--- resources/views/reports/all-gallinas.blade.php
+++ resources/views/reports/all-gallinas.blade.php
@@ -21,7 +21,7 @@
         <tbody>
             <tr>
                 <td>
-                    @if($item->gallinas_imagenes != null)
+                    @if($item->gallinas_imagenes->count() > 0)
                     <img src="../../files/gallinas/{{$item->id}}/{{$item->gallinas_imagenes->first()->imagen}}" class="h-100 w-100" alt="..." class="border">
                     @else
                     <img src="../../files/img/avatar-2.png" class="h-100 w-100" alt="..." class="border">
@@ -51,11 +51,11 @@
                         <tbody>
                             <tr>
                                 <td>
-                                    @if($item->gallos_hijos[0]->padre != null)
+                                    @if($item->gallos_hijos->count() > 0 && $item->gallos_hijos[0]->padre != null)
                                     <table class="table">
                                         <tr>
                                             <td>
-                                                @if($item->gallos_hijos[0]->padre->gallos_imagenes != null)
+                                                @if($item->gallos_hijos[0]->padre->gallos_imagenes->count() > 0)
                                                 <img src="../../files/gallos/{{$item->gallos_hijos[0]->padre->id}}/{{$item->gallos_hijos[0]->padre->gallos_imagenes->first()->imagen}}" width="100%" height="180" alt="..." class="border">
                                                 @else
                                                 <img src="../../files/img/avatar.png" width="100%" height="180" alt="..." class="border">
@@ -75,11 +75,11 @@
                                     @endif
                                 </td>
                                 <td>
-                                    @if($item->gallos_hijos[0]->madre != null)
+                                    @if($item->gallos_hijos->count() > 0 && $item->gallos_hijos[0]->madre != null)
                                     <table class="table">
                                         <tr>
                                             <td>
-                                                @if($item->gallos_hijos[0]->madre->gallinas_imagenes != null)
+                                                @if($item->gallos_hijos[0]->madre->gallinas_imagenes->count() > 0)
                                                 <img src="../../files/gallinas/{{$item->gallos_hijos[0]->madre->id}}/{{$item->gallos_hijos[0]->madre->gallinas_imagenes->first()->imagen}}" width="100%" height="180" alt="..." class="border">
                                                 @else
                                                 <img src="../../files/img/avatar-2.png" width="100%" height="180" alt="..." class="border">
