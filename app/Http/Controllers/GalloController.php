800249afdaa2d1786830c911e2482ca1ebf07651
gluis <luisfgb919@gmail.com>
2024-07-11 10:15:40 -0400

    Avances

--- app/Http/Controllers/GalloController.php
+++ app/Http/Controllers/GalloController.php
@@ -27,7 +27,7 @@ class GalloController extends Controller
         $g = Gallo::create($request->all())->latest('id')->first();
 
         if($request->padre_id != null){
-            $padre = Gallo::where('placa', $request->padre_id)->first();
+            $padre = Gallo::find($request->padre_id);
             $gh = GallosHijo::create([ 
                 'padre_id' => $padre->id,
                 'hijo_id' => $g->id,
@@ -36,7 +36,7 @@ class GalloController extends Controller
         }
 
         if($request->madre_id != null){
-            $madre = Gallina::where('placa', $request->madre_id)->first();
+            $madre = Gallina::find($request->madre_id);
             if(!empty($madre)){
                 $ghm = GallosHijo::find($gh->id);
                 $ghm->madre_id = $madre->id;
